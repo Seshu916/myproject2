61c0825ebc37a30cbc9149d29bb7c8e8b6fd17ef
Seshu916 <pavanseshu001@gmail.com>
2024-03-26 11:18:35 +0530

    inserted

---
+++ Condition3.java
@@ -0,0 +1,15 @@
+class Condition3{
+    public static void main(String[] args){
+        int age=19;
+        int weight=56;
+        if(age>18){
+            if(weight>55){
+               System.out.println("happliy donate blood");
+            }else{
+                System.out.println("weight not enough for blood donation");
+            }
+        }else{
+            System.out.println("age not enough for blood donation");
+        }
+    }
+}
---
+++ Switchex3.java
@@ -0,0 +1,25 @@
+class Switchex3{
+    public static void main(String[] args){
+        char c='a';
+        switch(c){
+            case 'a':
+                System.out.println("vowel");
+                break;
+            case 'e':
+                System.out.println("vowel");
+                break;
+            case 'i':
+                System.out.println("vowel");
+                break;
+            case 'o':
+                System.out.println("vowel");
+                break;
+            case 'u':
+                System.out.println("vowel");
+                break;
+            default:
+                System.out.println("consonanat");
+                break;
+        }
+    }
+}
